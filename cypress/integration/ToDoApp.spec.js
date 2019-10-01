6e3e64cddf87a6fca5eefbabec7ddb07ad349a28
John Tsevdos <tsevdosjohn@gmail.com>
2019-10-02 00:10:26 +0300

    Cypress tests

---
+++ cypress/integration/ToDoApp.spec.js
@@ -0,0 +1,28 @@
+describe("Todo app", () => {
+  it("contains correct titles", () => {
+    cy.visit("http://localhost:3000/05");
+    cy.contains("Testing");
+    cy.contains("My ToDos");
+  });
+
+  it("contains todo list", () => {
+    cy.get("#app ul > li").should("have.length", 3);
+
+    cy.contains("Learn React");
+    cy.contains("Go to Code.Hub");
+    cy.contains("Go out for a drink");
+  });
+
+  it("can add a new todo", () => {
+    cy.get("[data-testid=addTodo]")
+      .clear()
+      .type("Learn Redux");
+
+    cy.get("button")
+      .contains("Add ToDo")
+      .click();
+
+    cy.get("#app ul > li").should("have.length", 4);
+    cy.contains("Learn Redux");
+  });
+});
